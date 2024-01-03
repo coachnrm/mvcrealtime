df9896a65c5fc0e0d438742c46b54e863a13b768
Coach <nuttapong_coach@hotmail.com>
2024-01-03 17:17:25 +0700

    first comit

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using SignalRDemo3ytEFC.Data;
+using SignalRDemo3ytEFC.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SignalRDemo3ytEFC.Repositories
+{
+    public class ProductRepository
+    {
+        string connectionString;
+        private readonly ApplicationDbContext dbContext;
+
+
+        public ProductRepository(string connectionString, ApplicationDbContext _dbContext)
+        {
+            this.connectionString = connectionString;
+            this.dbContext = _dbContext;
+        }
+
+        public List<Product> GetProducts()
+        {
+            var prodList = dbContext.Product.ToList();
+            foreach (var emp in prodList)
+            {
+                dbContext.Entry(emp).Reload();
+            }
+            //var f = dbContext.Product.ToList();
+            return prodList;
+        }
+    }
+}
